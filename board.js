891bab3894a1a817180a7781b4da20f30b5af4cf
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2024-06-06 16:34:09 +0900

    카드 뒤집기 게임 구현
    2024-06-06 lightstar12
    1. 카드 div에 id가 제대로 부여되지 않는 현상 수정
    2. 플레이어 점수를 비교하여 승리 플레이어, 무승부 alert으로 출력 구현
    3. 변수명 수정

--- board.js
+++ board.js
@@ -9,26 +9,28 @@ const maxUserTurn = 2;
 // 플레이어가 카드를 고른 횟수
 let playerSelectCount = 0;
 // 선택한 카드의 인덱스를 담을 리스트
-let selectCardIdx = [];
+let selectCardIdxList = [];
 // 게임 판에서 뒤집어진 카드의 개수
 let flipedCardCount = 0;
+let playerScoreList = [0];
 // =============== 게임에서 필요한 변수
 // 카드들을 놓을 판 Div
 const $setCard = document.querySelector(".setCard");
 // 게임판에 배치된 카드의 수
 const cardCount = boardSize * boardSize;
 // 카드에 Id 값에 넣을 인덱스들 ※필요한 만큼 잘라서 사용(4x4 : 1번째 줄만, 5x5 : 2번째 줄까지, 6x6 : 전부)
-const totalCardIdIdx = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8",
+const totalCardIdIdxList = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8",
                  "9", "9", "10", "10", "11", "11", "12", "12",
                  "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18"];
                  // 카드의 Id값에 따라 부여할 색깔들을 담은 변수
 const cardColorList = ["red", "purple", "green", "olive", "blue", "orange", "chartreuse", "crimson", "darkblue", "darkgoldenrod",
                      "darkseagreen", "darkslategray", "darkturquoise", "deeppink", "dimgray", "indigo"];
 
-let cardIdIdx = totalCardIdIdx.slice(0, cardCount - 1);
+let cardIdIdxList = totalCardIdIdxList.slice(0, cardCount);
 // =============== 카드 조작에 필요한 변수
 // 카드를 뒤집는 함수
 const cardEvent1 = function flipCard() {
+  console.log(this.getAttribute("id"));
   const $cardList = document.querySelectorAll(`.cardDiv${boardSize}${boardSize}`);
   // 선택한 카드의 value 값이 "back"이라면
   if(this.getAttribute("value") === "back") {
@@ -39,7 +41,7 @@ const cardEvent1 = function flipCard() {
     // 선택한 카드의 인덱스를 저장함
     for (let i = 0; i < $cardList.length; i++) {
       if (this === $cardList[i]) {
-        selectCardIdx.push(i);
+        selectCardIdxList.push(i);
       }
     }
 
@@ -48,9 +50,9 @@ const cardEvent1 = function flipCard() {
 
     setTimeout(() => {
       if(playerSelectCount > 1) {
-        cardEvent2($cardList, selectCardIdx[0], selectCardIdx[1]);
-        count = 0;
-        selectCardIdx = [];
+        cardEvent2($cardList, selectCardIdxList[0], selectCardIdxList[1]);
+        playerSelectCount = 0;
+        selectCardIdxList = [];
       }
     }, 100);
   }
@@ -61,13 +63,13 @@ const cardEvent2 = function checkSameCard(cardList, idx1, idx2) {
   if(cardList[idx1].getAttribute("id") === cardList[idx2].getAttribute("id")) {
     alert("맞았습니다.");
     // 플레이어의 카드 총 맞춘 횟수
-    const playerFlipedCardCount = document.querySelector(`.player${userTurn}FlipedCardCountTd`);
+    const $playerFlipedCardCount = document.querySelector(`.player${userTurn}FlipedCardCountTd`);
     // 카드 맞춘 횟수
-    let collectCount = parseInt(playerFlipedCardCount.innerText);
+    let collectCount = parseInt($playerFlipedCardCount.innerText);
     // 하나 올림
     collectCount += 1;
     // 다시 innerText 넣음
-    playerFlipedCardCount.innerText = collectCount + "";
+    $playerFlipedCardCount.innerText = collectCount + "";
     // 뒤집혀진 카드 수를 2장 늘림
     flipedCardCount += 2;
   } else {
@@ -78,7 +80,19 @@ const cardEvent2 = function checkSameCard(cardList, idx1, idx2) {
 
   setTimeout(() => {
     if(flipedCardCount === cardCount) {
-      alert("게임이 끝났습니다.");
+      for (let i = 0; i < maxUserTurn; i++) {
+        playerScoreList.push(parseInt(document.querySelector(`.player${i + 1}FlipedCardCountTd`).innerText));
+      }
+      for (let i = 0; i < playerScoreList.length; i++) {
+        if (Math.max(...playerScoreList) === playerScoreList[i]) {
+          if (playerScoreList[i] === cardCount / 4) {
+            alert("무승부");
+          } else {
+            alert(`플레이어${i} 승리!`);
+          }
+          break;
+        }        
+      }
       // 새로고침하여 게임을 리셋함
       location.reload(true);
     }
@@ -103,17 +117,15 @@ const cardEvent3 = function returnCard(cardList, idx) {
 window.onload = function() {
   // 카드 무작위로 섞기
   for(let i = 0; i < 10; i++) {
-    const rNum1 = Math.floor(Math.random() * cardIdIdx.length);
-    const rNum2 = Math.floor(Math.random() * cardIdIdx.length);
+    const rNum1 = Math.floor(Math.random() * cardIdIdxList.length);
+    const rNum2 = Math.floor(Math.random() * cardIdIdxList.length);
 
-    const nTmp = cardIdIdx[rNum1];
-    cardIdIdx[rNum1] = cardIdIdx[rNum2];
-    cardIdIdx[rNum2] = nTmp;
+    const nTmp = cardIdIdxList[rNum1];
+    cardIdIdxList[rNum1] = cardIdIdxList[rNum2];
+    cardIdIdxList[rNum2] = nTmp;
   }
 
-  console.log(cardIdIdx);
-
-  let cardIdIdxCount = 0;
+  let cardIdIdxListCount = 0;
 
   for (let row = 0; row < boardSize; row++) {
     for (let col = 0; col < boardSize; col++) {
@@ -121,14 +133,13 @@ window.onload = function() {
       const cardDiv = document.createElement("div");
       cardDiv.setAttribute("class", `cardDiv${boardSize}${boardSize}`);
       cardDiv.setAttribute("value", "back");
-      cardDiv.setAttribute("id", `${cardIdIdx[cardIdIdxCount]}`);
+      cardDiv.setAttribute("id", `${cardIdIdxList[cardIdIdxListCount]}`);
       // 카드에 이벤트를 부여
       cardDiv.addEventListener("click", cardEvent1);
       // 지정해둔 판에 속성을 지정한 카드를 자식으로 생성
       $setCard.appendChild(cardDiv);
-      if (col % 2 == 1) {
-        cardIdIdxCount += 1;
-      }
+      
+      cardIdIdxListCount += 1;
     }
   }
 }
